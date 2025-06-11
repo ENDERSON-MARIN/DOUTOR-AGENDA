aeb82227aae6300cdbe73ba510c768b0e4c410e6
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-06-11 17:37:38 -0300

    feat: implement create clinic functionality with form validation and user session management

--- src/app/clinic-form/page.tsx
+++ src/app/clinic-form/page.tsx
@@ -1,5 +1,30 @@
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
 const ClinicFormPage = () => {
-  return <h1>Clinic Form Page</h1>;
+  return (
+    <div className="">
+      <Dialog open>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Adicionar Clinica</DialogTitle>
+              <DialogDescription>
+                Adicione uma clinica para continuar.
+              </DialogDescription>
+            </DialogHeader>
+            <ClinicForm/>
+          </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default ClinicFormPage;
