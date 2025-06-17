77138f1907ef0b3e792711c067b64936af9a0e3e
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-06-17 17:42:07 -0300

    fix: update clinic name display in AppSidebar and adjust sidebar button styles for better visual consistency

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -106,7 +106,7 @@ export function AppSidebar() {
                     <AvatarFallback>F</AvatarFallback>
                   </Avatar>
                   <div>
-                    {/* <p className="text-sm">{session.data?.user?.clinica?.}</p> */}
+                    <p className="text-sm">{session.data?.user?.clinic?.name}</p>
                     <p className="text-muted-foreground text-sm">
                       {session.data?.user.email}
                     </p>
