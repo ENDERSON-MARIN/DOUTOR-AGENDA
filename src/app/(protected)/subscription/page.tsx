9d4e2d5394423d1aa448e45b9ab0d0070c75e333
ENDERSON-MARIN <marinenderson1@gmail.com>
2025-07-16 23:28:06 -0300

    feat: ensure stripe session is created

--- src/app/(protected)/subscription/page.tsx
+++ src/app/(protected)/subscription/page.tsx
@@ -1,3 +1,6 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import {
   PageContainer,
   PageContent,
@@ -7,10 +10,17 @@ import {
   PageHeaderTitle,
 } from "@/components/ui/page-container";
 import { Separator } from "@/components/ui/separator";
+import { auth } from "@/lib/auth";
 
-import SubscriptionPlan from "./_components/subscription-plan";
+import { SubscriptionPlan } from "./_components/subscription-plan";
 
-export default function SubscriptionPage() {
+export default async function SubscriptionPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/login");
+  }
   return (
     <PageContainer>
       <PageHeaderContainer>
@@ -23,7 +33,7 @@ export default function SubscriptionPage() {
       </PageHeaderContainer>
       <Separator />
       <PageContent>
-        <SubscriptionPlan />
+        <SubscriptionPlan userEmail={session.user.email} />
       </PageContent>
     </PageContainer>
   );
